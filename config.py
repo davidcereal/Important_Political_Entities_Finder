5cc53b21f874345cf955e16fc57fe7b3d9eec3dc
David Berger <David@Davids-MBP-4.fios-router.home>
2015-12-27 15:59:55 -0500

    added config file

---
+++ config.py
@@ -0,0 +1,23 @@
+"""
+1. Store your Foreign Affairs login username and password. If you do not have an account, go to
+https://www.foreignaffairs.com and register for free. 
+
+Registered users without a subscription are entitled to 1 free article a month, so that is all that will 
+be scraped during the demo.
+
+2. If you do have a subscription, specify how many articles you would like to scrape and use as a corpus.
+Otherwise, keep at default of 1.
+
+3. Speficy path to downloaded chromdriver. For further details on this, please see the readme.md document. 
+Example: /Users/David/Downloads/chromedriver
+"""
+
+## Specify your username and password:
+login_username = ""
+login_password = ""
+
+## If you have a subcription, specify the number of articles to scrape:
+n_articles_to_scrape = 1
+
+## Specify path to chromedriver:
+path_to_chromedriver = ""
